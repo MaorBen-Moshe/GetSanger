7eea2f319c76f839b96891e7e052da2d493f48e5
maor9 <maor9920@gmail.com>
2021-03-02 17:41:42 +0200

    minors

--- GetSanger/GetSanger/UI pages/common/JobOfferPage.xaml.cs
+++ GetSanger/GetSanger/UI pages/common/JobOfferPage.xaml.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using GetSanger.ViewModels;
+using Xamarin.Essentials;
 
 namespace GetSanger.UI_pages.common
 {
@@ -15,18 +16,18 @@ namespace GetSanger.UI_pages.common
             BindingContext = new JobOfferViewModel();
         }
 
-        private void SendButton_Clicked(object sender, EventArgs e)
+        private async void SendButton_Clicked(object sender, EventArgs e)
         {
-            // send sms code
-            //try
-            //{
-            //    await Sms.ComposeAsync(new SmsMessage(Editorr.Text, Phone.Text));
-            //}
-            //catch
-            //{
-            //    await DisplayAlert("Error", "Could not send the message, please try again later", "OK");
-            //}
-           
+            //send sms code
+            try
+            {
+                await Xamarin.Essentials.Sms.ComposeAsync(new SmsMessage(Editorr.Text, Phone.Text));
+            }
+            catch
+            {
+                await DisplayAlert("Error", "Could not send the message, please try again later", "OK");
+            }
+
         }
     }
 }
