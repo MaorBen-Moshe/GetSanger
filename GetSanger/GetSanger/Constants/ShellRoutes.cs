1a053ec1e7d9a17ac64c0f59444b874709e52873
maor9 <maor9920@gmail.com>
2021-03-17 11:26:59 +0200

    continue building shell, add navigationService and shellRoutes

---
+++ GetSanger/GetSanger/Constants/ShellRoutes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetSanger.Constants
+{
+    public static class ShellRoutes
+    {
+        // here all the constants of the routes for the shell navigation
+    }
+}
---
+++ GetSanger/GetSanger/Services/NavigationService.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GetSanger.Services
+{
+    public class NavigationService
+    {
+        public NavigationService()
+        {
+
+        }
+
+        public void OpenMainPage()
+        {
+
+        }
+
+        public async void GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        public async Task NavigateTo(string i_ViewPath)
+        {
+            ShellNavigationState state = Shell.Current.CurrentState;
+            try
+            {
+                await Shell.Current.GoToAsync(i_ViewPath);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
