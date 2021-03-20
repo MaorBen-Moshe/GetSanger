bf80ccc5e2106d49f82e3b9da8379e80c0276c5d
4shachar <46491370+4shachar@users.noreply.github.com>
2021-03-20 05:29:15 +0200

    Added preliminary email registration for Android

---
+++ GetSanger/GetSanger.Android/Dependencies/Auth.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GetSanger.Interfaces;
+using System.Threading.Tasks;
+using Android.Gms.Extensions;
+using Firebase.Auth;
+using GetSanger.Droid.Dependencies;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Auth))]
+namespace GetSanger.Droid.Dependencies
+{
+    class Auth : IAuth
+    {
+        public async Task<string> GetIdToken()
+        {
+            FirebaseUser user = FirebaseAuth.Instance.CurrentUser;
+
+            if (user == null)
+            { 
+                await FirebaseAuth.Instance.SignInAnonymouslyAsync();
+                user = FirebaseAuth.Instance.CurrentUser;
+            }
+
+            return user.GetIdToken(true).Result.ToString();
+        }
+
+        public Task<string> GetUserId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GetSanger/GetSanger/Interfaces/IAuth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSanger.Interfaces
+{
+    public interface IAuth
+    {
+        Task<string> GetIdToken();
+        Task<string> GetUserId();
+    }
+}
