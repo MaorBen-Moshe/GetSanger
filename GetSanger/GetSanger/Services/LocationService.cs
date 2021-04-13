795634b69e5a102d29e04a19a4008f7b848899b9
maor9 <maor9920@gmail.com>
2021-04-13 17:31:02 +0300

    clean location service

--- GetSanger/GetSanger/Services/LocationService.cs
+++ GetSanger/GetSanger/Services/LocationService.cs
@@ -62,53 +62,51 @@ namespace GetSanger.Services
             m_Timer.Stop();
         }
 
-
-        // -----------------------------------------------------------------
-        // When the sanger starts his location sharing on the map,  HelperLocationUpdate method runs in a separate thread on the sanger device,
-        // and sends his location every few seconds through the server, and the client updates his map pin accordingly.
-
-
-        public void StartMapTracking()
-        {
-            Thread t = new Thread(() => HelperLocationUpdate());
-            t.Start();
-        }
-
-        public void HelperLocationUpdate()
-        {
-            Timer timer = new Timer(new TimerCallback(SangerLocationUpdate), null, 0, 5000);
-        }
+        //// -----------------------------------------------------------------
+        //// When the sanger starts his location sharing on the map,  HelperLocationUpdate method runs in a separate thread on the sanger device,
+        //// and sends his location every few seconds through the server, and the client updates his map pin accordingly.
 
 
-        public async void SangerLocationUpdate(object o_State)
-        {
-            if (!KeepThread)
-                Cancelation();
-            m_CurrLocation = await GetCurrentLocation();
-            if(m_CurrLocation != m_PrevLocation && m_CurrLocation != null)
-            {
-                // Send m_CurrLocation to server (need to send SangerID and ClientID?)
+        //public void StartMapTracking()
+        //{
+        //    Thread t = new Thread(() => HelperLocationUpdate());
+        //    t.Start();
+        //}
+
+        //public void HelperLocationUpdate()
+        //{
+        //    Timer timer = new Timer(new TimerCallback(SangerLocationUpdate), null, 0, 5000);
+        //}
+
+
+        //public async void SangerLocationUpdate(object o_State)
+        //{
+        //    if (!KeepThread)
+        //        Cancelation();
+        //    m_CurrLocation = await GetCurrentLocation();
+        //    if(m_CurrLocation != m_PrevLocation && m_CurrLocation != null)
+        //    {
+        //        // Send m_CurrLocation to server (need to send SangerID and ClientID?)
                 
-            }
+        //    }
            
-        }
+        //}
 
-        // Client side:
-        public void HelperClientActivityMapUpdate(Location o_Location)
-        {
-            Thread t = new Thread(() => ClientActivityMapUpdate(ref o_Location));
-            t.Start();
-        }
+        //// Client side:
+        //public void HelperClientActivityMapUpdate(Location o_Location)
+        //{
+        //    Thread t = new Thread(() => ClientActivityMapUpdate(ref o_Location));
+        //    t.Start();
+        //}
 
-        // This method requests from the server the updated location of the sanger, 
-        //    we might want to use FCM instead, so it will be handled like and event
-        public void ClientActivityMapUpdate(ref Location o_Location)
-        {
-            // Get Sanger location from the server
-            Location SangerLocation = null; // Swap this line with real SangerLocation from server
+        //// This method requests from the server the updated location of the sanger, 
+        ////    we might want to use FCM instead, so it will be handled like and event
+        //public void ClientActivityMapUpdate(ref Location o_Location)
+        //{
+        //    // Get Sanger location from the server
+        //    Location SangerLocation = null; // Swap this line with real SangerLocation from server
 
-            o_Location = SangerLocation;
-        }
-        
+        //    o_Location = SangerLocation;
+        //}
     }
 }
