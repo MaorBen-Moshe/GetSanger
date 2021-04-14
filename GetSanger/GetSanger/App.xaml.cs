bbdc055c9733a70634ef4ed0bfcf9298b44bef21
Golan Aharoni <golan3g@gmail.com>
2021-04-14 12:18:49 +0300

    fixing master for merge

--- GetSanger/GetSanger/App.xaml.cs
+++ GetSanger/GetSanger/App.xaml.cs
@@ -2,7 +2,6 @@ using Xamarin.Forms;
 using GetSanger.AppShell;
 using GetSanger.UI_pages.signup;
 using GetSanger.UI_pages.common;
-using Plugin.FirebasePushNotification;
 
 namespace GetSanger
 {
@@ -11,32 +10,7 @@ namespace GetSanger
         public App()
         {
             InitializeComponent();
-
             MainPage = new SignupCategoriesPage();
-
-            // IOS Push
-            // Token event
-            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
-            {
-                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
-            };
-            // Push message received event
-            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
-            {
-
-                System.Diagnostics.Debug.WriteLine("Received");
-
-            };
-            //Push message received event
-            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
-            {
-                System.Diagnostics.Debug.WriteLine("Opened");
-                foreach (var data in p.Data)
-                {
-                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
-                }
-
-            };
         }
 
         protected override void OnStart()
