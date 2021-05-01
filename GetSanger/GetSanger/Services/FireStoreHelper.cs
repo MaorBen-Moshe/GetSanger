7857de00de2b92ea948fada9dd5f12f69f06189d
4shachar <46491370+4shachar@users.noreply.github.com>
2021-05-01 23:19:57 +0300

    implemented GetAllJobOffers

--- GetSanger/GetSanger/Services/FireStoreHelper.cs
+++ GetSanger/GetSanger/Services/FireStoreHelper.cs
@@ -168,20 +168,25 @@ namespace GetSanger.Services
         public static async Task<List<JobOffer>> GetAllJobOffers(List<Category> i_Categories = null)
         {
             // if i_Categories == null than get all job offers in data base else filter by the categories
-            string uri = "uri here";
-            Dictionary<string, List<Category>> id = new Dictionary<string, List<Category>>
+            string uri = "https://europe-west3-get-sanger.cloudfunctions.net/GetAllJobOffers";
+            Dictionary<string, List<Category>> categoriesDictionary = new Dictionary<string, List<Category>>
             {
-                ["categories"] = i_Categories
+                ["Categories"] = i_Categories
             };
 
-            string json = JsonSerializer.Serialize(id);
-            HttpResponseMessage response = await HttpClientService.SendHttpRequest(uri, json, HttpMethod.Post);
+            string json = JsonSerializer.Serialize(categoriesDictionary);
+            string idToken = await AuthHelper.GetIdTokenAsync();
+
+            HttpResponseMessage response = await HttpClientService.SendHttpRequest(uri, json, HttpMethod.Post, idToken);
             if (!response.IsSuccessStatusCode)
             {
                 throw new Exception(await response.Content.ReadAsStringAsync());
             }
 
-            return JsonSerializer.Deserialize<List<JobOffer>>(await response.Content.ReadAsStringAsync());
+            List<JobOffer> jobOffers = JsonSerializer.Deserialize<List<JobOffer>>(await response.Content.ReadAsStringAsync());
+            convertDateTimeToLocalTime(jobOffers);
+
+            return jobOffers;
         }
 
         public static async Task<JobOffer> GetJobOffer(string i_JobId)
