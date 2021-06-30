86ea22a380dbc377a467fad02aea646cbde00304
maor9 <maor9920@gmail.com>
2021-06-30 18:45:36 +0300

    ; error

--- GetSanger/GetSanger/Interfaces/ICrashlytics.cs
+++ GetSanger/GetSanger/Interfaces/ICrashlytics.cs
@@ -7,7 +7,7 @@ namespace GetSanger.Interfaces
     public interface ICrashlytics
     {
         void SetCustomKeys(Dictionary<string, object> i_Dictionary);
-        public void SetCustomKey(string key, object value)
+        public void SetCustomKey(string key, object value);
         void AddCustomLogMessage(string i_Message);
         void SetUserId(string i_UserId);
         void RecordException(Exception i_Exception);
