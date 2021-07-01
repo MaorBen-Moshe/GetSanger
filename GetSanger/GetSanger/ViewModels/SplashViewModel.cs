1208b09c039bf1c055b493248bc2487ae1e35d07
maor9 <maor9920@gmail.com>
2021-07-01 14:46:48 +0300

    add splash view model

---
+++ GetSanger/GetSanger/ViewModels/SplashViewModel.cs
@@ -0,0 +1,57 @@
+﻿using GetSanger.Extensions;
+using GetSanger.Services;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace GetSanger.ViewModels
+{
+    public class SplashViewModel : BaseViewModel
+    {
+        private bool m_ConnectivtyChanged = false;
+
+        public SplashViewModel()
+        {
+        }
+
+        public async override void Appearing()
+        {
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+            try
+            {
+                if (Connectivity.NetworkAccess.Equals(NetworkAccess.None) == false)
+                {
+                    await login();
+                }
+                else
+                {
+                    await r_PageService.DisplayAlert("ERROR", "No Connection.", "Try again");
+                }
+            }
+            catch (Exception e)
+            {
+                await e.LogAndDisplayError(nameof(SplashViewModel), "Error", e.Message);
+            }
+        }
+
+        public void Disappearing()
+        {
+            Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
+        }
+
+        private async void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (m_ConnectivtyChanged == false && e.NetworkAccess.Equals(NetworkAccess.Internet) == true)
+            {
+                m_ConnectivtyChanged = true;
+                await login();
+            }
+        }
+
+        private Task login()
+        {
+            AppManager.Instance.Services.SetDependencies();
+            return r_LoginServices.TryAutoLogin();
+        }
+    }
+}
--- GetSanger/GetSanger/Views/SplashPage.xaml.cs
+++ GetSanger/GetSanger/Views/SplashPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using GetSanger.Extensions;
-using GetSanger.Services;
-using System;
-using System.Threading.Tasks;
-using Xamarin.Essentials;
+﻿using GetSanger.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,47 +14,16 @@ namespace GetSanger.Views
             InitializeComponent();
         }
 
-        protected async override void OnAppearing()
+        protected override void OnAppearing()
         {
-            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
             base.OnAppearing();
-            try
-            {
-                if(Connectivity.NetworkAccess.Equals(NetworkAccess.None) == false)
-                {
-                    await login();
-                }
-                else
-                {
-                    await DisplayAlert("ERROR", "No Connection.", "Try again");
-                }
-            }
-            catch(Exception e)
-            {
-                await e.LogAndDisplayError(nameof(SplashPage), "Error", e.Message);
-            }
+            (BindingContext as BaseViewModel).Appearing();
         }
 
         protected override void OnDisappearing()
         {
-            Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
+            (BindingContext as SplashViewModel).Disappearing();
             base.OnDisappearing();
-        }
-
-        private async void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
-        {
-            if(m_ConnectivtyChanged == false && e.NetworkAccess.Equals(NetworkAccess.Internet) == true)
-            {
-                m_ConnectivtyChanged = true;
-                await login();
-            }
-        }
-
-        private Task login()
-        {
-            AppManager.Instance.Services.SetDependencies();
-            LoginServices login = AppManager.Instance.Services.GetService(typeof(LoginServices)) as LoginServices;
-            return login.TryAutoLogin();
         }
     }
 }
